d19a7799334775960d6ebf1e61b3fa4a4ea2472f
cosammalaika <cosammalaika@gmail.com>
2024-04-27 12:04:32 +0200

    Changes

--- lib/screen/hymn_list_screen.dart
+++ lib/screen/hymn_list_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/services.dart';
 import 'package:ecz_hynms/widgets/drawer_widget.dart';
 import 'package:flutter/material.dart';
 import '../database/database_helper.dart';
-import '../database/hymn_search.dart';
 import '../models/hymn.dart';
 import 'about_screen.dart';
 import 'hymn_detail_screen.dart';
